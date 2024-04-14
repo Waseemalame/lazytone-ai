2b13c08291cd0a1513527937678797b374064403
Waseemalame <waseemalame45@gmail.com>
2024-04-13 19:19:27 -0700

    rename folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "chatbot",
+    "api",
     "rest_framework",
     "corsheaders"
 ]
