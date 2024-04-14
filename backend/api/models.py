b01b1733e777acec236b2fa42333f01a406605f5
Waseemalame <waseemalame45@gmail.com>
2024-04-13 21:24:09 -0700

    Added Conversation and Message data models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+    startedat = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name='conversations', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Conversation {self.id} started at {self.start_time}"
+
+class Message(models.Model):
+  conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+  text = models.TextField()
+  sender_type = models.CharField(max_length=10, choices=(('user', 'User'), ('ai', 'AI')))
+  createdat = models.DateTimeField(auto_now_add=True)
+  updatedat = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return f"Message from {self.sender_type} at {self.createdat}: {self.text}"
