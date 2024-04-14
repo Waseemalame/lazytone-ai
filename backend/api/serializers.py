640b893507a196397b0d04b3f0196dfa1333d5ab
Waseemalame <waseemalame45@gmail.com>
2024-04-13 21:25:13 -0700

    Add serializers for User, Message, and Conversation models

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Message, Conversation
 
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
@@ -8,5 +9,17 @@ class UserSerializer(serializers.ModelSerializer):
     extra_kwargs = {"password": {"write_only": True}}
 
   def create(self, validated_data):
-    user = User.objects.create_users(**validated_data)
+    user = User.objects.create_user(**validated_data)
     return user
+
+class MessageSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Message
+    fields = ["id", "conversation", "user", "sender_type", "text", "createdat", "updatedat"]
+    extra_kwards = {"user": {"read_only": True}}
+
+class ConversationSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Conversation
+    fields = ["id", "user", "text", "createdat", "updatedat"]
+    extra_kwards = {"user": {"read_only": True}}
