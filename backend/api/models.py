93cb791a760dd2f12cbb9b4a36b12cf2ea16b920
Waseemalame <waseemalame45@gmail.com>
2024-04-16 17:54:33 -0700

    updated data model and migrations for Messages

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,15 +6,17 @@ class Conversation(models.Model):
     user = models.ForeignKey(User, related_name='conversations', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Conversation {self.id} started at {self.start_time}"
+        return f"Conversation {self.id} started at {self.startedat}"
 
+# Todo: Remove nulls=True after testing
 class Message(models.Model):
-  conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
-  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
-  text = models.TextField()
-  sender_type = models.CharField(max_length=10, choices=(('user', 'User'), ('ai', 'AI')))
-  createdat = models.DateTimeField(auto_now_add=True)
-  updatedat = models.DateTimeField(auto_now=True)
+  conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE, null=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages", null=True)
+  text = models.TextField(null=True)
+  audio_file = models.FileField(upload_to='messages_audios/', null=True, blank=True)
+  sender_type = models.CharField(max_length=10, null=True, choices=(('user', 'User'), ('ai', 'AI')))
+  createdat = models.DateTimeField(auto_now_add=True, null=True)
+  updatedat = models.DateTimeField(auto_now=True, null=True)
 
   def __str__(self):
-      return f"Message from {self.sender_type} at {self.createdat}: {self.text}"
+      return f"Message from {self.user} at {self.createdat}: {self.text}"
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('conversations/', views.ConversationListCreate.as_view(), name='conversation-list-create'),
     path('conversations/<int:pk>/', views.ConversationDelete.as_view(), name='conversation-delete'),
     path('conversations/<int:conversation_id>/messages/', views.MessageListCreate.as_view(), name='message-list-create'),
+    path('conversations/<int:conversation_id>/messages/process/', views.OpenAIResponseView.as_view(), name='openai-process'),
+    path('messages/', views.MessageListCreate.as_view(), name='message-list'),
 ]
